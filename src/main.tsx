5ed7d996cc0077016682e4be9e1a219587397ad2
DamirLut <d.amlutstudio@gmail.com>
2023-02-15 12:13:31 +0500

    VKWebAppInit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { ConfigProvider, AdaptivityProvider } from "@vkontakte/vkui";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "@vkontakte/vkui/dist/vkui.css";
-import "./index.css";
+import { ConfigProvider, AdaptivityProvider } from '@vkontakte/vkui';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import bridge from '@vkontakte/vk-bridge';
+import '@vkontakte/vkui/dist/vkui.css';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <ConfigProvider>
-      <AdaptivityProvider>
-        <App />
-      </AdaptivityProvider>
-    </ConfigProvider>
+bridge.send('VKWebAppInit');
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ConfigProvider>
+    <AdaptivityProvider>
+      <App />
+    </AdaptivityProvider>
+  </ConfigProvider>,
 );
